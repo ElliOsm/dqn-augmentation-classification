0057e08f295e0e4950e19f05e3ea9153855db7f8
Elli Eirini Osmantzikidou <elli.osmantzikidou@gmail.com>
2021-06-08 16:04:51 +0300

    added pre-pre-processing

--- data_prossesing/classifier_utils.py
+++ data_prossesing/classifier_utils.py
@@ -5,3 +5,8 @@ def get_default_device():
         return torch.device('cuda')
     else:
         return torch.device('cpu')
+
+
+# def accuracy(outputs, labels):
+#     _, preds = torch.max(outputs, dim=1)
+#     return torch.tensor(torch.sum(preds == labels).item() / len(preds)), preds
--- data_prossesing/data_pytorch.py
+++ data_prossesing/data_pytorch.py
@@ -21,11 +21,70 @@ def one_hot_encodoing(x):
     return F.one_hot(x, params['num_class'])
 
 
+#
+# def data_reader(dir):
+#     image_dataset = datasets.ImageFolder(dir)
+#
+#     img_csv = pd.read_csv(dir + '/train.csv')
+#
+#
+#     data_aug_transform = transforms.Compose([
+#         transforms.Resize([int(params['img_height']), int(params['img_width'])]),
+#         transforms.RandomResizedCrop(224),
+#         transforms.RandomHorizontalFlip(),
+#         # converts all images to (0,1]
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406],
+#                              [0.229, 0.224, 0.225])
+#     ])
+#     data_transform = transforms.Compose([
+#         transforms.Resize([int(params['img_height']), int(params['img_width'])]),
+#         # converts all images to (0,1]
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406],
+#                              [0.229, 0.224, 0.225])
+#     ])
+#
+#     train_size = int(0.7 * len(image_dataset))
+#     val_size = len(image_dataset) - train_size
+#     # print('Size:')
+#     # print('train length: ', train_size)
+#     # print('val length: ', val_size)
+#     subset_train, subset_val = random_split(image_dataset, [train_size, val_size])
+#
+#     # print('Subset:')
+#     # print('train length: ', len(subset_train))
+#     # print('val length: ', len(subset_val))
+#
+#     train_dataset = DataloaderSubsetDataset(subset_train, dir, transform=data_aug_transform)
+#     valid_dataset = DataloaderSubsetDataset(subset_val, dir, transform=data_transform)
+#
+#     # print('Dataset:')
+#     # print('train length: ', train_dataset.__len__())
+#     # print('val length: ', valid_dataset.__len__())
+#
+#
+#     train_dataset_loader = torch.utils.data.DataLoader(train_dataset,
+#                                                        batch_size=params['batch_size'],
+#                                                        shuffle=True,
+#                                                        drop_last=True)
+#
+#     valid_dataset_loader = torch.utils.data.DataLoader(valid_dataset,
+#                                                        batch_size=params['batch_size'],
+#                                                        shuffle=True,
+#                                                        drop_last=True)
+#     # print('Dataloaders:')
+#     # print('train length: ', train_dataset.__len__())
+#     # print('val length: ', valid_dataset.__len__())
+#
+#     dataloaders = {'train': train_dataset_loader,
+#                    'val': valid_dataset_loader}
+#
+#     return dataloaders
+#
+
 def data_reader(dir):
     image_dataset = datasets.ImageFolder(dir)
-
-    img_csv = pd.read_csv(dir + '/train.csv')
-
 
     data_aug_transform = transforms.Compose([
         transforms.Resize([int(params['img_height']), int(params['img_width'])]),
@@ -44,7 +103,7 @@ def data_reader(dir):
                              [0.229, 0.224, 0.225])
     ])
 
-    train_size = int(0.7 * len(image_dataset))
+    train_size = int(0.9 * len(image_dataset))
     val_size = len(image_dataset) - train_size
     # print('Size:')
     # print('train length: ', train_size)
@@ -55,13 +114,12 @@ def data_reader(dir):
     # print('train length: ', len(subset_train))
     # print('val length: ', len(subset_val))
 
-    train_dataset = DataloaderSubsetDataset(subset_train, dir, transform=data_aug_transform)
-    valid_dataset = DataloaderSubsetDataset(subset_val, dir, transform=data_transform)
-
+    train_dataset = DataloaderSubsetDataset(subset_train, transform=data_aug_transform)
+    valid_dataset = DataloaderSubsetDataset(subset_val, transform=data_transform)
+    #
     # print('Dataset:')
     # print('train length: ', train_dataset.__len__())
     # print('val length: ', valid_dataset.__len__())
-
 
     train_dataset_loader = torch.utils.data.DataLoader(train_dataset,
                                                        batch_size=params['batch_size'],
@@ -80,4 +138,3 @@ def data_reader(dir):
                    'val': valid_dataset_loader}
 
     return dataloaders
-
