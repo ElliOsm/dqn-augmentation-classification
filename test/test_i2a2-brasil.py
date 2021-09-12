322fa200bee4cef3705af7f708fb49a9a8b0353e
ElliOsm <elli.osmantzikidou@gmail.com>
2021-09-12 11:35:13 +0300

    Chore: add solo test file

---
+++ test/test_i2a2-brasil.py
@@ -0,0 +1,32 @@
+import time
+import torch
+from thesis.model.ResNet50_classifier import ResNet50
+from thesis.data_prossesing.data_pytorch import data_reader,get_default_device
+import os
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import matplotlib.pyplot as plt
+
+import copy
+
+CUDA_LAUNCH_BLOCKING = 1
+
+# set device
+device = get_default_device()
+print("Device:", device)
+
+model = ResNet50()
+model.freeze()
+model.to(device)
+
+# load dataset
+data_dir = '../../../../../Desktop/thesis/thesis/thesis/thesis/data/train_i2a2_complete/data'
+dataloaders = data_reader(data_dir)
+
+weight_dir = os.path.join('../..', 'weights', 'test_i2a2-brasil.hdf5')
+load_weight = model.load_state_dict(torch.load(weight_dir))
+
+
+prediction = model.test_model(dataloaders['test'], device)
