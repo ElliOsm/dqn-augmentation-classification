470e219bca18bfccdcfde6351b6ac3c60566f008
ElliOsm <elli.osmantzikidou@gmail.com>
2021-09-12 11:31:10 +0300

    Chore: add basic qlearning

---
+++ model/qlearning.py
@@ -0,0 +1,150 @@
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import transforms
+
+from thesis.model.ResNet50_classifier import ResNet50
+
+
+class DQNAgent:
+
+    def __init__(self):
+        self.actions = {
+            0: self.action1,
+            1: self.action2,
+            2: self.action3
+        }
+        self.qTable = np.zeros((2, 3))  # Q = np.zeros((state_size, action_size))
+        self.state = [0, 1]
+        self.learning_rate = 0.4
+        self.discount_factor = 0.3
+        self.episodes = 50
+
+    #
+    # # horizontal flip
+    # def action3(self, image):
+    #     unorm = UnNormalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    #     unorm(image)
+    #
+    #     transform = transforms.Compose([
+    #         transforms.ToPILImage()
+    #     ])
+    #     image = transform(image)
+    #     image = transforms.functional.hflip(img=image)
+    #     transform = transforms.Compose([
+    #         transforms.ToTensor(),
+    #         transforms.Normalize([0.485, 0.456, 0.406],
+    #                              [0.229, 0.224, 0.225])
+    #     ])
+    #
+    #     return transform(image)
+
+    # adjust_contrast
+    def action1(self, image):
+        image = np.squeeze(image)
+
+        transform = transforms.Compose([
+            transforms.ToPILImage()
+        ])
+        image = transform(image)
+        image = transforms.functional.adjust_contrast(image, contrast_factor=3)
+        transform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+        image = transform(image)
+        image = torch.unsqueeze(image, 0)
+
+        return image
+
+    # equalize image
+    def action2(self, image):
+        image = np.squeeze(image)
+
+        transform = transforms.Compose([
+            transforms.ToPILImage()
+        ])
+        image = transform(image)
+        image = transforms.functional.adjust_brightness(img=image, brightness_factor=2)
+        transform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+        image = transform(image)
+        image = torch.unsqueeze(image, 0)
+
+        return image
+
+    # sharpness
+    def action3(self, image):
+        image = np.squeeze(image)
+
+        transform = transforms.Compose([
+            transforms.ToPILImage()
+        ])
+        image = transform(image)
+        image = transforms.functional.adjust_sharpness(img=image, sharpness_factor=10)
+        transform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+        image = transform(image)
+        image = torch.unsqueeze(image, 0)
+        return image
+
+    def select_random_action(self):
+        return random.randint(0, 2)
+
+    def apply_action(self, action_num, image):
+        action = self.actions[action_num]
+        image = action(image)
+        return image
+
+    def get_reward(self, m_before, m_after):
+        difference = (m_after - m_before).float()
+        if difference > 0:
+            return 1
+        elif difference == 0:
+            return 0
+        else:
+            return -1
+
+    def get_state(self, reward):
+        if reward > 0:
+            return 0
+        else:
+            return 1
+
+    def update_QTable(self, state, action_num, reward):
+        # Q[state, action] = Q[state, action] + lr * (reward + gamma * np.max(Q[new_state, :]) — Q[state, action])
+        self.qTable[state, action_num] = self.qTable[state, action_num] + self.learning_rate * (
+                reward + self.discount_factor * max(self.qTable[state]) - self.qTable[state, action_num])
+
+    def get_features(self, features):
+        return features.std()
+
+    def find_best_action(self, image, model):
+        features = model.extract_features(image)
+        print("features",features)
+        m_before = self.get_features(features)
+        print(m_before)
+        exit()
+        for e in range(self.episodes):
+            action_num = self.select_random_action()
+            image_after = self.apply_action(action_num, image)
+            features_after = model.extract_features(image_after.to("cuda"))
+            m_after = self.get_features(features_after)
+            reward = self.get_reward(m_before, m_after)
+            state = self.get_state(reward)
+            # print("------------------------------------")
+            # print("Repeat: ", e)
+            # print("State: ", state)
+            # print("Action: ", action_num)
+            # print("Reward: ", reward)
+            # print("------------------------------------")
+            self.update_QTable(state, action_num, reward)
+        print("*************************************")
+        print("Q-table")
+        print(self.qTable)
+        print("*************************************")
+
+    def choose_final_action(self):
+        return np.where(self.qTable == np.amax(self.qTable))[1][0]
---
+++ try/test_train_qlearning.py
@@ -0,0 +1,80 @@
+# from thesis.model import drq
+import time
+import torch
+
+from scipy.ndimage.interpolation import rotate
+
+from thesis.model.qlearning import DQNAgent
+from thesis.model.ResNet50_classifier import ResNet50
+from thesis.data_prossesing.data_pytorch import data_reader,print_image,get_default_device
+import os
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import matplotlib.pyplot as plt
+import hydra
+
+import yaml
+import copy
+
+
+
+CUDA_LAUNCH_BLOCKING = 1
+
+with open('../config.yaml', 'r') as f:
+    params = yaml.safe_load(f)
+
+# set device
+device = get_default_device()
+print("Device:", device)
+
+# load dataset
+data_dir = '../../../../Desktop/thesis/thesis/thesis/thesis/data/train_i2a2_complete/data'
+dataloaders = data_reader(data_dir)
+
+data = next(iter(dataloaders['test']))
+data2 = next(iter(dataloaders['test']))
+
+# resnet creation
+model = ResNet50()
+model.freeze()
+model.to(device)
+
+# weight load
+weight_dir = os.path.join('..', 'weights', 'test_i2a2-brasil.hdf5')
+load_weight = model.load_state_dict(torch.load(weight_dir))
+
+
+#agent creation
+agent = DQNAgent()
+correct_after = 0
+correct_before = 0
+total = 0
+all_after = 0
+
+model.eval()
+for inputs, labels in dataloaders['test']:
+    inputs = inputs.to(device)
+    labels = labels.to(device)
+    predicted_label_before = model(inputs)
+    _, preds = torch.max(predicted_label_before, dim=1)
+    if (preds != labels):
+        agent.find_best_action(inputs, model)
+        action = agent.choose_final_action()
+        image_after = agent.apply_action(action, inputs)
+        
+        predicted_label_after = model(inputs)
+        _, preds_after = torch.max(predicted_label_after, dim=1)
+
+        if preds_after == labels:
+            correct_after = correct_after + 1
+        all_after += labels.size(0)
+    else:
+        correct_before = correct_before + 1
+    total += labels.size(0)
+
+print("Correct without RL: ", correct_before,"/", total)
+print("Correct with RL: ", correct_after + correct_before , "/", total)
+print("Correct only RL: ", correct_after , "/", all_after)
+print("All Correct: ", correct_before + correct_after , "/", total)
